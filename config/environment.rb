4459f0e109aeb078235224fd69301455d6b46c5c
Alfio <alfioc87@hotmail.it>
2014-02-07 10:06:39 +0100

    UPDATE .gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings = {
     :port => 587,
     :domain => 'mail.live.com',
     :user_name => 'wicity@live.it',
-    :password => '!ra73on19!',
+    :password => '',
     :authentication => 'plain',
     :enable_starttls_auto => true
 }
