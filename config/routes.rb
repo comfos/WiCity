31671db2a046b36fc54c538bb36554517c62afe1
Alfio <alfioc87@hotmail.it>
2014-02-18 19:52:37 +0100

    BUGFIX logout

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ WiCity::Application.routes.draw do
   get '/nearserv', to: 'home#nearserv'
 
   get '/users/sign_in', to: 'home#index'
+  get '/users/sign_out', to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
