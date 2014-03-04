02e21c3b00a78729d8db9fbafab6adf5d19e0cb5
Alfio <alfioc87@hotmail.it>
2014-03-04 12:58:34 +0100

    EDIT Posti

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,16 @@
 class Park < ActiveRecord::Base
+
+  validate :occupati_cannot_be_greater_than_capacity
+
+  validates :occupati, numericality: true
+  validates :capacity, numericality: true
+
+  def occupati_cannot_be_greater_than_capacity
+    if !occupati.nil? && !capacity.nil?
+      if occupati > capacity
+        errors.add(:occupati, "can't be greater than capacity")
+      end
+    end
+  end
+
 end
