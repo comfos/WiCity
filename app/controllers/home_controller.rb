530deb9b49b27874862292017c2d229aea27bc29
Alfio <alfioc87@hotmail.it>
2014-12-08 16:11:09 +0100

    UPDATE nearservices

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,9 @@ class HomeController < ApplicationController
     @pharms = Pharm.all
     @parks = Park.all
     @taxistations = Taxistation.all
+    @radiotaxis = Radiotaxi.all
     @restaurants = Restaurant.all
+
   end
 
 end
